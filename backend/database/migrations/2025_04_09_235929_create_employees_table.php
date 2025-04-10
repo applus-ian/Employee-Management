7bac4bfc282b7e858ae75a7fbd6d1017d075ebd1
Pongporopong <158302746+Pongporopong@users.noreply.github.com>
2025-04-10 10:59:23 +0800

    feat: add model and migration for employee table

---
+++ backend/database/migrations/2025_04_09_235929_create_employees_table.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+            $table->string('suffix')->nullable();
+            $table->enum('gender', ['male', 'female', 'other'])->nullable();
+            $table->date('birthdate');
+            $table->enum('civil_status', [
+                'single',
+                'married',
+                'widowed',
+                'divorced',
+                'separated',
+                'anulled',
+                // 'cohabiting', should we add cohabiting/live-in?
+            ]);
+            $table->string('nationality');
+            $table->string('region');
+            $table->string('province');
+            $table->string('city_or_municipality');
+            $table->string('barangay');
+            $table->string('street');
+            $table->string('phone_number')->unique();
+            $table->string('emergency_contact1');
+            $table->string('emergency_contact2');
+            $table->string('email')->unique();
+            $table->unsignedBigInteger('job_position_id');
+            $table->date('date_hired');
+            $table->enum('employment_type', [
+                'fulltime',
+                'parttime',
+                'contractual',
+                // what other employment type
+            ]);
+            $table->unsignedBigInteger('manager_id');
+            $table->string('profile_pic_url')->nullable();
+
+            $table->foreign('job_position_id')->references('id')->on('job_positions')->onDelete('cascade');
+            $table->foreign('manager_id')->references('id')->on('employees')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
