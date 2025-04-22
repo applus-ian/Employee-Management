4baa20f3c71313cd7cf3fb4834c3ff964779e8b1
Pongporopong <158302746+Pongporopong@users.noreply.github.com>
2025-04-23 07:53:56 +0800

    feat(controller): Add employee controller

---
+++ backend/app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateEmployeeRequest;
+use App\Http\Requests\UpdateEmployeeRequest;
+use App\Models\Employee;
+use App\Services\EmployeeService;
+use App\Services\EmploymentStatusHistoryService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    protected $employeeService, $employmentStatusHistoryService;
+
+    public function __construct(EmployeeService $employeeService, EmploymentStatusHistoryService $employmentStatusHistoryService)
+    {
+        $this->employeeService = $employeeService;
+        $this->employmentStatusHistoryService = $employmentStatusHistoryService;
+    }
+
+    // Create Employee Method
+    public function create(CreateEmployeeRequest $request): JsonResponse
+    {
+        $new_employee = $this->employeeService->createEmployee($request->validated());
+
+        $this->employmentStatusHistoryService->createEmploymentStatusHistory($new_employee->id, 'employee_created',$new_employee->date_hired);
+
+        return response()->json(['message' => 'Employee created successfully!'], 201);
+    }
+
+    // Update Employee Method
+    public function update(UpdateEmployeeRequest $request, $employee_id): JsonResponse
+    {
+        // Decode the employee ID
+        $id = $this->employeeService->decodeEmployeeId($employee_id);
+
+        // If the ID is invalid (null), return a custom error message and status code
+        if ($id === null) {
+            return response()->json(['message' => 'Invalid ID provided!'], 400); // 400 Bad Request
+        }
+
+        // Find the employee by ID
+        $employee =$this->employeeService->getEmployeeById($id);
+
+        // If the employee is not found, return a 404 response
+        if (!$employee) {
+            return response()->json(['message' => 'Employee not found!'], 404);
+        }
+
+        // Proceed with updating the employee's details
+        $updated_employee = $this->employeeService->updateEmployee($employee, $request->validated());
+
+        // Return the updated employee as a JSON response
+        return response()->json($updated_employee, 200);
+    }
+
+}
