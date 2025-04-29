e61ba9a21c776a03090a7730513615470c64cd63
Pongporopong <158302746+Pongporopong@users.noreply.github.com>
2025-04-29 09:49:02 +0800

    refactor(backend): Update default data seeders

--- backend/database/seeders/JobPositionSeeder.php
+++ backend/database/seeders/JobPositionSeeder.php
@@ -15,6 +15,32 @@ class JobPositionSeeder extends Seeder
     {
         JobPosition::insert([
             ['title' => 'Super Admin'],
+
+            ['title' => 'Country Manager'],
+            ['title' => 'Finance ERP Functional Specialist'],
+            ['title' => 'Procurement Specialist'],
+            ['title' => 'HR Officer'],
+            ['title' => 'Admin Executive'],
+            ['title' => 'Expense Auditor'],
+
+            ['title' => 'IT Manager'],
+            ['title' => 'Senior Software Developer'],
+            ['title' => 'Software Developer'],
+            ['title' => 'Software QA Automation Tester'],
+            ['title' => 'System Administrator'],
+
+            ['title' => 'Lead HR Administrator'],
+            ['title' => 'HR Administrator'],
+            ['title' => 'Project Manager'],
+            ['title' => 'Software QA Systems Tester'],
+
+            ['title' => 'FSS-Cebu Delivery Lead'],
+            ['title' => 'RTR Lead'],
+            ['title' => 'PTP/OTC Lead'],
+            ['title' => 'RTR Specialist'],
+            ['title' => 'RTR/IC Specialist'],
+            ['title' => 'PTP Specialist'],
+            ['title' => 'OTC Specialist'],
         ]);
     }
 }
