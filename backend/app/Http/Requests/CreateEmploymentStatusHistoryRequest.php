82b8b595b1c2efeb789eff2a8f48593c1a7cc880
Lynn Tenebroso Sanchez <lynntenebrososanchez2002@gmail.com>
2025-04-30 10:22:43 +0800

    feat(employment-status-history/request): authorize create and update requests

--- backend/app/Http/Requests/CreateEmploymentStatusHistoryRequest.php
+++ backend/app/Http/Requests/CreateEmploymentStatusHistoryRequest.php
@@ -11,7 +11,7 @@ class CreateEmploymentStatusHistoryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- backend/app/Http/Requests/UpdateEmploymentStatusHistoryRequest.php
+++ backend/app/Http/Requests/UpdateEmploymentStatusHistoryRequest.php
@@ -11,7 +11,7 @@ class UpdateEmploymentStatusHistoryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
